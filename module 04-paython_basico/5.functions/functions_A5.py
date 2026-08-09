4ac591ceafc0b5d13a6146616358ff26cabc82f1
Alejandro Martinez <alejo792@icloud.com>
2026-08-09 14:13:09 -0700

    Add function to split, sort, and join strings

--- module 04-paython_basico/5.functions/functions_A5.py
+++ module 04-paython_basico/5.functions/functions_A5.py
@@ -1,6 +1,21 @@
+def clean_up_string(text):
+    my_string = text.split("-")
+    my_string.sort()
+    return my_string
+
+def convert_list_to_string(clear_text):
+    result = "-".join(clear_text)
+    return result
+
+
+
+
 def main ():
     my_string = "python-variable-funcion-computadora-monitor"
-    clear_string =my_string.split("-")
-    my_string_list =[clear_string]
-    print(my_string_list)
+    clean_text = clean_up_string(my_string)
+    result = convert_list_to_string(clean_text)
+    print(result)
+
 main()
+
+
